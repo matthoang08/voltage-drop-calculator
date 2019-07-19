b5afe675ad52fb61590d82d2872f82ae2388f6c8
Matt Hoang <matthoang08@gmail.com>
2019-07-18 19:04:59 -0500

    bug: fix build issues

--- src/components/VoltageDropCalculator.tsx
+++ src/components/VoltageDropCalculator.tsx
@@ -4,7 +4,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import From from 'react-bootstrap/Form';
 import Form from 'react-bootstrap/Form';
 
 export const VoltageDropCalculator: React.FunctionComponent = () => {
